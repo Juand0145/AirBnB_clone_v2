241d7beb9a7b53d31950ea446224dbab674a203f
Juand0145 <juand0145@gmail.com>
2021-09-11 19:47:33 -0500

    Adding task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """List of states file"""
+
 from flask import Flask, render_template
 from models import storage, State
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/states_list")
 def state_list():
     """Is a methdo display a HTML page"""
     return render_template("7-states_list.html",
@@ -19,4 +21,4 @@ def storage_close(var=None):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port="5000")
