1bd76ce2cb7c77050d3388c93680911d1a6eb3f6
Juand0145 <juand0145@gmail.com>
2021-09-11 19:11:39 -0500

    fixing pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,12 +8,15 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def state_list():
     """Is a methdo display a HTML page"""
-    return render_template("7-states_list.html", data=storage.all(State).values())
+    return render_template("7-states_list.html",
+                           data=storage.all(State).values())
+
 
 @app.teardown_appcontext
 def storage_close(var=None):
     """Close the SQL database"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
