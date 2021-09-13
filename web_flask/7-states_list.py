52840a4bb2c1fb5124944497bde19d7aaa9ef813
Juand0145 <juand0145@gmail.com>
2021-09-13 11:03:04 -0500

    Fixing docuemntation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""List of states file"""
+"""7-states_list module
+Starts a Flask web application
+"""
 
 from flask import Flask, render_template
 from models import storage, State
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/states_list")
 def state_list():
-    """Is a methdo display a HTML page"""
+    """Displays HTML page
+    H1 tag: States
+    UL tag: list of all State objects in DBStorage sorted by name
+    """
     return render_template("7-states_list.html",
                            data=storage.all(State).values())
 
 
 @app.teardown_appcontext
 def storage_close(var=None):
-    """Close the SQL database"""
+    """Removes current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port="5000")
+    app.run(host='0.0.0.0')
